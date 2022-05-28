494dde70055d55ba165f15d8801ccf29fec23c2a
Andrés Mata <andres_mata21@outlook.com>
2022-05-28 17:03:17 -0500

    Obtención de los datos envíados al endpoint del Post

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -13,8 +13,13 @@ const usuarioPut = (req, res = response) => {
 }
 
 const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
     res.json({
-        msg: 'post API - controlador'
+        msg: 'post API - controlador',
+        nombre,
+        edad
     });
 }
 
