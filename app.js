0e71800bd7ea8dc0c2594b19a094ce64cd63429b
Andrés Mata <andres_mata21@outlook.com>
2022-05-28 12:37:23 -0500

    Método con rutas y el escucha en el servidor

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 require('dotenv').config();
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
-});
-
-app.listen(process.env.PORT, () => {
-    console.log('servidor corriendo en puerto', process.env.PORT);
-});
