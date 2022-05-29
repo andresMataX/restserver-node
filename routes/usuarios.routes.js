0a14d5c215adce5a482a37356f29bd8ce03c8347
Andrés Mata <andres_mata21@outlook.com>
2022-05-29 18:43:30 -0500

    Validación de los campos para registrar a un usuario

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -11,7 +11,10 @@ router.put('/:id', usuarioPut);
 
 // Definimos un middleware para la validación que estará acumulando los errores para para response
 router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe de ser más de 6 letras').isLength({ min: 6 }),
     check('correo', 'El correo no es válido').isEmail(),
+    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE'])
 ], usuariosPost);
 
 router.delete('/', usuariosDelete);
