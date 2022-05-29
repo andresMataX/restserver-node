47defea4c9f195b6f6a6f10b9831f0730ab8bb78
Andrés Mata <andres_mata21@outlook.com>
2022-05-29 18:08:54 -0500

    Validación del correo

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -2,6 +2,7 @@ const { response, request } = require("express");
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+const { validationResult } = require("express-validator");
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -26,12 +27,22 @@ const usuarioPut = (req = request, res = response) => {
 
 const usuariosPost = async (req = request, res = response) => {
 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
+
     const { nombre, correo, password, rol } = req.body;
 
     const usuario = new Usuario({ nombre, correo, password, rol });
 
     // Verificar si el correo existe
-
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'Ese correo ya está registrado'
+        });
+    }
 
     // Encriptar la contraseña
     // Número de vueltas que será la encriptación (10 por defecto)
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { usuariosGet, usuarioPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller');
 
 const router = Router();
@@ -8,7 +9,10 @@ router.get('/', usuariosGet);
 
 router.put('/:id', usuarioPut);
 
-router.post('/', usuariosPost);
+// Definimos un middleware para la validación que estará acumulando los errores para para response
+router.post('/', [
+    check('correo', 'El correo no es válido').isEmail(),
+], usuariosPost);
 
 router.delete('/', usuariosDelete);
 
