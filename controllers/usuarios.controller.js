9a73ee78afb335f03df45fbbd946eb03177f93a0
Andrés Mata <andres_mata21@outlook.com>
2022-05-29 17:46:46 -0500

    Guardado de usuario en la base de datos

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,4 +1,5 @@
 const { response, request } = require("express");
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -23,12 +24,14 @@ const usuarioPut = (req = request, res = response) => {
 
 const usuariosPost = (req = request, res = response) => {
 
-    const { nombre, edad } = req.body;
+    const body = req.body;
+
+    const usuario = new Usuario(body);
+    usuario.save();
 
     res.json({
         msg: 'post API - controlador',
-        nombre,
-        edad
+        usuario
     });
 }
 
