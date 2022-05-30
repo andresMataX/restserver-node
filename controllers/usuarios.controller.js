afbd08b6ae7716f37a4867da0b0c70a445395e24
Andrés Mata <andres_mata21@outlook.com>
2022-05-29 21:59:07 -0500

    Limitación al momento de enviar los usuarios

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -6,8 +6,8 @@ const Usuario = require('../models/usuario');
 const usuariosGet = async (req = request, res = response) => {
 
     // const { nombre = 'No Name', apellido } = req.query;
-
-    const usuarios = await Usuario.find();
+    const { limite = 5 } = req.query;
+    const usuarios = await Usuario.find().limit(Number(limite));
 
     res.json({ usuarios });
 }
